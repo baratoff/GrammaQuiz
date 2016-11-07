9f07518a1cbc32a51d141f9f52e4c0a004e17c4f
baratoff <johann84bjarni@gmail.com>
2016-11-07 14:56:27 +0100

    less and first component working, need gulp

---
+++ BaseForAngular2/app/Navibar/component.ts
@@ -0,0 +1,7 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'quiz-navibar',
+    template: '<h1>Navibar</h1>'
+})
+export class NavibarComponent { }
--- BaseForAngular2/app/app.module.ts
+++ BaseForAngular2/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpModule } from "@angular/http";
+import { NavibarComponent } from "./Navibar/component"
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+    imports: [
+        BrowserModule,
+        HttpModule
+    ],
+    declarations: [
+        AppComponent,
+        NavibarComponent
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
 export class AppModule { }
